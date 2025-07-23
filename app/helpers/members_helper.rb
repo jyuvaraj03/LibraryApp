efec467407eb103519dfeaf3036c8aa6c74bfc09
Yuvaraj J <jyuvaraj03@gmail.com>
2025-07-24 00:41:09 +0530

    DRY new member form field

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,18 @@
 module MembersHelper
+  # DRY form field helper for member form
+  def member_form_field(form, field, type, label_text: nil, required: false, placeholder: nil, min: nil, max: nil)
+    label_classes = "block mb-2 font-medium"
+    label_classes += " required-field-label" if required
+    input_classes = "shadow border px-4 py-2 w-full rounded"
+    label_text ||= t("#{field}_label")
+    input_options = { class: input_classes }
+    input_options[:required] = true if required
+    input_options[:placeholder] = placeholder if placeholder
+    input_options[:min] = min if min
+    input_options[:max] = max if max
+    content_tag :div, class: "flex flex-col mb-6" do
+      form.label(field, label_text, class: label_classes) +
+      form.send(type, field, input_options)
+    end
+  end
 end
---
+++ test/helpers/members_helper_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class MembersHelperTest < ActionView::TestCase
+  test 'member_form_field renders label and input' do
+    member = Member.new
+    form_builder = ActionView::Helpers::FormBuilder.new(:member, member, self, {})
+    html = member_form_field(form_builder, :name, :text_field, required: true, label_class: 'required-field-label', placeholder: 'Name')
+    assert_includes html, 'required-field-label'
+    assert_includes html, 'placeholder="Name"'
+    assert_includes html, 'type="text"'
+    assert_includes html, 'name="member[name]"'
+  end
+end
