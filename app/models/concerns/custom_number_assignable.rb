68bafc74c6943e0ee4e3c330c69f30ffc4870bd7
Yuvaraj J <jyuvaraj03@gmail.com>
2025-07-25 09:51:47 +0530

    Address review comments

--- app/models/concerns/custom_number_assignable.rb
+++ app/models/concerns/custom_number_assignable.rb
@@ -3,6 +3,8 @@ module CustomNumberAssignable
 
   included do
     before_validation :ensure_custom_number
+
+    validates :custom_number, presence: true, uniqueness: true
   end
 
   private
@@ -11,14 +13,19 @@ module CustomNumberAssignable
   # Example: 'C000012' for prefix 'C', length 6
   def ensure_custom_number
     col = self.class.try(:custom_number_column) || :custom_number
-
     return if self[col].present?
 
     prefix = self.class.try(:custom_number_prefix) || 'C'
     length = self.class.try(:custom_number_length) || 6
+
     self.class.transaction do
-      last_pattern = self.class.order(col).lock(true).last&.send(col)
-      last_number = last_pattern ? last_pattern.gsub("#{prefix}", '').to_i : 0
+      last_pattern = self.class
+        .where("#{col} LIKE ?", "#{prefix}%")
+        .order(Arel.sql("CAST(SUBSTRING(#{col} FROM LENGTH('#{prefix}') + 1) AS INTEGER)"))
+        .lock(true)
+        .last&.send(col)
+
+      last_number = last_pattern ? last_pattern.gsub(/^#{Regexp.escape(prefix)}/, '').to_i : 0
       new_pattern = "#{prefix}#{(last_number + 1).to_s.rjust(length, '0')}"
       self[col] = new_pattern
     end
